e96b84566ddd760d6c7eb42dcd33dcb0e5e46fae
Sam ManekShah <159655170+samthemanekshahw@users.noreply.github.com>
2025-02-03 04:26:47 +0530

    Create fvbgdfrg.js

---
+++ fvbgdfrg.js
@@ -0,0 +1,9 @@
+// simple.js
+
+// Define a function that greets the user by name
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Call the greet function with the name "World"
+greet("World");
